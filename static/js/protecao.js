6df1343449342ee818351c75add7f8d2dbc92be9
israelazvd <israelazevedo700@hotmail.com>
2025-10-08 01:02:49 -0300

    Atualização das telas

--- static/js/protecao.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-  const u = localStorage.getItem('usuarioLogado');
-  if(!u){ alert('Faça login'); window.location.href='/templates/tela_login.html'; }
-});
